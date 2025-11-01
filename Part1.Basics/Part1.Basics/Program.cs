d6fdc991d7ae9d8865d0e476b2535842b48ebe17
ferdibayram <ferdigit@gmail.com>
2025-11-01 03:07:45 +0300

    cont

--- Part1.Basics/Part1.Basics/Program.cs
+++ Part1.Basics/Part1.Basics/Program.cs
@@ -4,15 +4,15 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //Console.WriteLine(typeof(string));
-        //Console.WriteLine(typeof(int));
-        //Console.WriteLine(short.MaxValue);
-        //double result = Math.Pow(2, 15t);
-        //Console.WriteLine(result);
-        //(int yas, bool medeniHal, string isim) kisi = (32, false, "ferdi");
-        //Console.WriteLine(kisi.isim);
-        //const int yas = 32;  
-        ////yas = 35; // it won't work
+        Console.WriteLine(typeof(string));
+        Console.WriteLine(typeof(int));
+        Console.WriteLine(short.MaxValue);
+        double result = Math.Pow(2, 15);
+        Console.WriteLine(result);
+        (int yas, bool medeniHal, string isim) kisi = (32, false, "ferdi");
+        Console.WriteLine(kisi.isim);
+        const int yas = 32;
+        //yas = 35; // it won't work
         //int yas2 = 34;
         //yas2 = 36;
         //Console.WriteLine(yas2);
