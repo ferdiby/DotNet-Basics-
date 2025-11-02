34d7b7f4b74cec09045f56beb628059fca4ff34d
ferdibayram <ferdigit@gmail.com>
2025-11-02 04:01:04 +0300

    cont

--- Part1.Basics/Part1.Basics/Program.cs
+++ Part1.Basics/Part1.Basics/Program.cs
@@ -4,14 +4,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine(typeof(string));
-        Console.WriteLine(typeof(int));
-        Console.WriteLine(short.MaxValue);
-        double result = Math.Pow(2, 15);
-        Console.WriteLine(result);
-        (int yas, bool medeniHal, string isim) kisi = (32, false, "ferdi");
-        Console.WriteLine(kisi.isim);
-        const int yas = 32;
+        //Console.WriteLine(typeof(string));
+        //Console.WriteLine(typeof(int));
+        //Console.WriteLine(short.MaxValue);
+        //double result = Math.Pow(2, 15);
+        //Console.WriteLine(result);
+        //(int yas, bool medeniHal, string isim) kisi = (32, false, "ferdi");
+        //Console.WriteLine(kisi.isim);
+        //const int yas = 32;
         //yas = 35; // it won't work
         //int yas2 = 34;
         //yas2 = 36;
@@ -34,9 +34,61 @@ internal class Program
         //object name3 = "bayram";
         //string name4 = (string) name3;
         //Console.WriteLine(name4);
+        // var keyword-dynamic keyword
 
-        // var keyword
+        // Type-Conversion 
+        //string medeniHal = "true";
+        //bool medeniHal2 = bool.Parse(medeniHal);
+        //ternary operator
+        //string evliMiDegilMi = medeniHal2 ? "Sen evlisin" : "Evli değilsin";
+        //Console.WriteLine(evliMiDegilMi);
 
+        //string tcKimlikNo = "12345";
+        //int tcKimlikUser = int.Parse(tcKimlikNo);
+        //Console.WriteLine(tcKimlikUser+7);
 
+        //bool medeniHal3 = Convert.ToBoolean(medeniHal);
+        //Console.WriteLine(medeniHal3);
+
+        //tostring()
+        //int x = 765;
+        //string y = x.ToString();
+        //Console.WriteLine(y+"ay");
+
+        //ASCII values -- Implicit Coverion
+        //char c = '%';
+        //int b = (int)c;
+        //char d = '-';
+        //int e = (int)d;
+        //Console.WriteLine(b+d);
+
+        //Explicit Conversion
+        //int x = 65;
+        //char y = (char)x;
+        //Console.WriteLine(y);
+
+        //Logic operators
+
+        //bool decisionUser = false;
+        //int decResult = ! decisionUser ? 7 : 20;
+        //Console.WriteLine(decResult);
+
+        //String a = "ayşe";
+        //string b = a;
+        //Console.WriteLine(a+b);
+
+        //int x = 5;
+        //string y = x.ToString();
+        //Console.WriteLine(y+4);
+        //Console.WriteLine(default(int)); //0
+        //Console.WriteLine(default(double)); //0
+        //Console.WriteLine(default(string)); //null
+        //Console.WriteLine(default(char)); //nul
+        //Console.WriteLine(default(DateTime)); 
+        //Console.WriteLine(default(bool)); //false
+        //Console.WriteLine(default(Type)); //null
+        //Type t = typeof(DateTime);
+        
     }
+
 }
