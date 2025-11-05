449e39fa51f4612ed13cab30fe967c73960123b3
ferdibayram <ferdigit@gmail.com>
2025-11-05 05:53:46 +0300

    reorganized sloutions and projects

--- Part1.Basics/Strings/Program.cs
+++ Part1.Basics/Strings/Program.cs
@@ -4,7 +4,35 @@
     {
         #region String
 
+        //List<string> cities = new List<string>() { "ankara", "istanbul", "tekirdağ", "bursa", "maraş" };
+        //foreach (string city in cities)
+        //{
+        //    if (city.Length > 7)
+        //        Console.WriteLine(city);
+        //}
+
+        //string name = "murat";
+        //List<char> sortedName = new List<char>();
+
+        //foreach (char c in name)
+        //{
+        //    sortedName.Add(c);
+        //}
+
+        //sortedName.Sort();
+
+        //foreach (var item in sortedName)
+        //{
+        //    Console.Write($"{item}");
+        //}
+
+        
+
+
+
+
+
         #endregion
 
     }
-} 
+}
